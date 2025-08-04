c2215e55fb08950d91988d5f603428b698d62d96
Ali Ahmed <121296033+Ali-Ahmed18@users.noreply.github.com>
2025-08-04 20:12:52 +0500

    ProjectBtn_Link

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -3,12 +3,13 @@ import Link from "next/link";
 import { HiArrowRight } from "react-icons/hi2";
 const ProjectsBtn = () => {
   return (
-    <div className="mx-auto xl:mx-0">
-      <Link
+    <Link
         href={"/work"}
         className="relative w-[185px] h-[185px] flex justify-center items-center 
         bg-circleStar bg-cover bg-center bg-no-repeat group"
       >
+    <div className="mx-auto xl:mx-0">
+      
         <Image
           src={"/rounded-text.png"}
           width={141}
@@ -17,8 +18,9 @@ const ProjectsBtn = () => {
           className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
         />
         <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300" />
-      </Link>
+      
     </div>
+  </Link>
   );
 };
 
